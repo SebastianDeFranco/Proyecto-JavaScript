fbdfb14e28e9c3c798bcc97b4faee7209297395b
SebastianDeFranco <sebastianedefranco@gmail.com>
2021-06-08 18:26:55 -0300

    agregando DOM

--- script.js
+++ script.js
@@ -1,78 +1,65 @@
-// const productos = [
-//     {id: 1, nombre:"Surf Break Tee (White)", tipo:"Remeras", precio:3200},
-//     {id: 2, nombre:"Peace No Love Tee (Tie Dye)", tipo:"Remeras", precio:3650},
-//     {id: 3, nombre:"Ripntail Tee (Grey Lightning Wash)", tipo:"Remeras", precio:4800},
-//     {id: 4, nombre:"Nerm Street Tee (Multi Spiral Dye)", tipo:"Remeras", precio:3650},
-//     {id: 5, nombre:"Prisma Embroidered Tee (Blue Lightning Wash)", tipo:"Remeras", precio:4800},
-//     {id: 6, nombre:"Lord Nermal Pocket Tee (Spiral Dye)", tipo:"Remeras", precio:3650},
-//     {id: 7, nombre:"Ripndip Teddy Fresh Packable Anorak Jacket", tipo:"Buzos", precio:11800},
-//     {id: 8, nombre:"Nerm Street Hooded Coach Jacket (Black)", tipo:"Buzos", precio:11700},
-//     {id: 9, nombre:"Nermzilla Hooded Coach Jacket (Baby Blue)", tipo:"Buzos", precio:11700},
-//     {id: 10, nombre:"Heaven And Hell Puffer Jacket (Blue)", tipo:"Camperas", precio:17100},
-//     {id: 11, nombre:"Field Of Cats Sherpa Jacket (Black)", tipo:"Camperas", precio:11700},
-//     {id: 12, nombre:"RIPNTAIL Sherpa Jacket (Tie Dye)", tipo:"Camperas", precio:12800},
-//     {id: 13, nombre:"Great Wave Face Mask (Multi)", tipo:"Accesorios", precio:1950},
-//     {id: 14, nombre:"Nerm Plush Chair", tipo:"Accesorios", precio:532800},
-//     {id: 15, nombre:"Lord Nermal Plush Keychain (White)", tipo:"Accesorios", precio:1600},
-// ];
+let productos = [
+    {id: 1, nombre:"Surf Break Tee (White)", imagen:"../img/remera-1.jpg" , precio:3200},
+    {id: 2, nombre:"Peace No Love Tee (Tie Dye)", imagen:"../img/remera-2.jpg", precio:3650},
+    {id: 3, nombre:"Ripntail Tee (Grey Lightning Wash)", imagen:"../img/remera-3.jpg", precio:4800},
+    {id: 4, nombre:"Nerm Street Tee (Multi Spiral Dye)", imagen:"../img/remera-4.jpg", precio:3650},
+    {id: 5, nombre:"Prisma Embroidered Tee (Blue Lightning Wash)", imagen:"../img/remera-5.jpg", precio:4800},
+    {id: 6, nombre:"Lord Nermal Pocket Tee (Spiral Dye)", imagen:"../img/remera-6.jpg", precio:3650},
+    {id: 7, nombre:"Ripndip Teddy Fresh Packable Anorak Jacket", imagen:"../img/buzo-1.jpg", precio:11800},
+    {id: 8, nombre:"Nerm Street Hooded Coach Jacket (Black)", imagen:"../img/buzo-2.jpg", precio:11700},
+    {id: 9, nombre:"Nermzilla Hooded Coach Jacket (Baby Blue)", imagen:"../img/buzo-3.jpg", precio:11700},
+    {id: 10, nombre:"Heaven And Hell Puffer Jacket (Blue)", imagen:"../img/campera-1.jpg", precio:17100},
+    {id: 11, nombre:"Field Of Cats Sherpa Jacket (Black)", imagen:"../img/campera-2.jpg", precio:11700},
+    {id: 12, nombre:"RIPNTAIL Sherpa Jacket (Tie Dye)", imagen:"../img/campera-3.jpg", precio:12800},
+    {id: 13, nombre:"Great Wave Face Mask (Multi)", imagen:"../img/accesorio-1.jpg", precio:1950},
+    {id: 14, nombre:"Nerm Plush Chair", imagen:"../img/accesorio-2.jpg", precio:532800},
+    {id: 15, nombre:"Lord Nermal Plush Keychain (White)", imagen:"../img/accesorio-3.jpg", precio:1600},
+    {id: 16, nombre:"Gluten Free Hoodie (Black)", imagen:"../img/coleccion-1.jpg", precio:8600},
+    {id: 17, nombre:"Gluten Free Tee (Natural)", imagen:"../img/coleccion-2.jpg", precio:3250},
+    {id: 18, nombre:"Rubber Logo Hoodie (Sunburst Spiral Tie Dye)", imagen:"../img/coleccion-3.jpg", precio:9700},
+];
+
+const listaProductos = document.getElementById("contenedor");
+
+productos.forEach(prod => {
+    let card = document.createElement('div');
+    card.innerHTML = `
+    <div class ="card">
+    <img src="${prod.imagen}" alt="">
+    <h2 class ="titulo-secundario">${prod.nombre}<h2>
+    <p class="parrafo-card">$${prod.precio}</p>
+    <span" class ="pointer" id="prod${prod.id}">
+    <span class="material-icons">add_shopping_cart</span>
+    </span>
+</div>`
 
 
-// const Ingresar = parseInt(prompt("Ingrese el monto que quiere gastar"));
-// const Filtro = productos.filter(elemento => elemento.precio <= Ingresar);
-// console.log(Filtro);
 
-// class Ropa{
-// constructor (nombre, tipo, precio) {
-//     this.nombre = nombre;
-//     this.tipo   = tipo;
-//     this.precio  = precio;
-// }
-// }
+listaProductos.appendChild(card);
 
-// const ropa1 = new Ropa("Surf Break Tee (White)", "Remeras", 3200);
-// const ropa2 = new Ropa("Peace No Love Tee (Tie Dye)", "Remeras", 3650);
-// const ropa3 = new Ropa("Ripntail Tee (Grey Lightning Wash)","Remeras", 4800);
-// const ropa4 = new Ropa("Nerm Street Tee (Multi Spiral Dye)", "Remeras", 3650);
-// const ropa5 = new Ropa("Prisma Embroidered Tee (Blue Lightning Wash)","Remeras", 4800);
-// const ropa6 = new Ropa("Lord Nermal Pocket Tee (Spiral Dye)", "Remeras", 3650);
-// const ropa7 = new Ropa("Ripndip Teddy Fresh Packable Anorak Jacket", "Buzos", 11800);
-// const ropa8 = new Ropa("Nerm Street Hooded Coach Jacket (Black)", "Buzos", 11700);
-// const ropa9 = new Ropa("Nermzilla Hooded Coach Jacket (Baby Blue)", "Buzos", 11700);
-// const ropa10 = new Ropa("Heaven And Hell Puffer Jacket (Blue)", "Camperas",17100);
-// const ropa11 = new Ropa("Field Of Cats Sherpa Jacket (Black)", "Camperas",11700);
-// const ropa12 = new Ropa("RIPNTAIL Sherpa Jacket (Tie Dye)", "Camperas",12800);
-// const ropa13 = new Ropa("Great Wave Face Mask (Multi)", "Accesorios", 1950);
-// const ropa14 = new Ropa("Nerm Plush Chair", "Accesorios", 532800);
-// const ropa15 = new Ropa("Lord Nermal Plush Keychain (White)", "Accesorios",1600);
+const shop = document.getElementById("shop");
 
-// let Mostrar = prompt("Que prenda desea?")
+let boton = document.getElementById("prod"+ prod.id)
+boton.addEventListener('click', ()=>{
+    
+    let badge = document.createElement('Span');
+    badge.innerHTML = `
+    <span class="icon-button__badge"></span>
+    `
+    shop.appendChild(badge);
+    
 
-// if(Mostrar == "Remeras"){
-//     console.log(ropa1);
-//     console.log(ropa2);
-//     console.log(ropa3);
-//     console.log(ropa4);
-//     console.log(ropa5);
-//     console.log(ropa6);
-// } else if(Mostrar == "Buzos"){
-//     console.log(ropa7);
-//     console.log(ropa8);
-//     console.log(ropa9);
-// } else if(Mostrar== "Camperas"){
-//     console.log(ropa10);
-//     console.log(ropa11);
-//     console.log(ropa12);
-// }else if (Mostrar== "Accesorios"){
-//     console.log(ropa13);
-//     console.log(ropa14);
-//     console.log(ropa15);
-// }else{
-//     console.log("No se encontro el Producto");
-// }
+    console.log(prod.nombre +" $" + prod.precio);
+    
+})
+
+})
 
 
-// let Carrito = [];
-// let total = 0;
+
+
+
+
 
 
 
